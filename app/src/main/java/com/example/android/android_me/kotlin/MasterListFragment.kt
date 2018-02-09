90fcddac58b95396d21153edd3c7c4f9481ec431
Samuel Kwak <kwak123@gmail.com>
2018-02-09 17:32:34 -0500

    masterlistfragment as kt

--- app/src/main/java/com/example/android/android_me/kotlin/MasterListFragment.kt
+++ app/src/main/java/com/example/android/android_me/kotlin/MasterListFragment.kt
@@ -1,10 +1,12 @@
 package com.example.android.android_me.kotlin
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.GridView
 import com.example.android.android_me.R
 
@@ -24,8 +26,24 @@ class MasterListFragment : Fragment() {
         val rootView: View = inflater?.inflate(R.layout.fragment_master_list, container, false) as View
         val gridView: GridView = rootView.findViewById(R.id.images_grid_view) as GridView
 
+        gridView.onItemClickListener = ImageGridViewClickListener()
 
         return rootView
     }
 
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+
+        try {
+            imageClickListener = activity as OnImageClickListener
+        } catch (e: ClassCastException) {
+            throw ClassCastException(context?.toString() + "must implement OnImageClickListener interface")
+        }
+    }
+
+    private inner class ImageGridViewClickListener : AdapterView.OnItemClickListener {
+        override fun onItemClick(adapterView: AdapterView<*>?, view: View?, position: Int, l: Long) {
+            imageClickListener.onImageSelected(position)
+        }
+    }
 }
