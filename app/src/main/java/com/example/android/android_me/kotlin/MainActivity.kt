885e3265741b2eccc08a1141bb62ba35015fd2b9
Samuel Kwak <kwak123@gmail.com>
2018-02-09 18:09:03 -0500

    basic conversion complete

--- app/src/main/java/com/example/android/android_me/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/android/android_me/kotlin/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.android.android_me.kotlin
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
 import android.widget.GridView
 import com.example.android.android_me.R
+import com.example.android.android_me.data.AndroidImageAssets
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MasterListFragment.OnImageClickListener {
 
     companion object {
         private val HEAD: Int = 0
@@ -34,8 +36,75 @@ class MainActivity : AppCompatActivity() {
             gridView.numColumns = 2
 
             if (savedInstanceState == null) {
+                val headFragment = BodyPartFragment()
+                headFragment.setImageIds(AndroidImageAssets.getHeads())
+                headFragment.setListIndex(headIndex)
+
+                val bodyFragment = BodyPartFragment()
+                bodyFragment.setImageIds(AndroidImageAssets.getBodies())
+                bodyFragment.setListIndex(bodyIndex)
+
+                val legFragment = BodyPartFragment()
+                legFragment.setImageIds(AndroidImageAssets.getLegs())
+                legFragment.setListIndex(legIndex)
+
+                supportFragmentManager.beginTransaction()
+                        .add(R.id.head_container, headFragment)
+                        .add(R.id.body_container, bodyFragment)
+                        .add(R.id.leg_container, legFragment)
+                        .commit()
+            }
+        }
+    }
+
+    override fun onImageSelected(imagePosition: Int) {
+        val partType: Int = imagePosition / 12
+        val partIndex: Int = imagePosition - 12 * partType
+
+        if (twoPane) {
+            val newFragment = BodyPartFragment()
+            newFragment.setListIndex(partIndex)
+
+            when (partType) {
+                HEAD -> {
+                    newFragment.setImageIds(AndroidImageAssets.getHeads())
+                    supportFragmentManager.beginTransaction()
+                            .replace(R.id.head_container, newFragment)
+                            .commit()
+                }
+                BODY -> {
+                    newFragment.setImageIds(AndroidImageAssets.getBodies())
+                    supportFragmentManager.beginTransaction()
+                            .replace(R.id.body_container, newFragment)
+                            .commit()
+                }
+                LEG -> {
+                    newFragment.setImageIds(AndroidImageAssets.getLegs())
+                    supportFragmentManager.beginTransaction()
+                            .replace(R.id.leg_container, newFragment)
+                            .commit()
+                }
+                else -> {}
 
             }
+        } else {
+            when (partType) {
+                HEAD -> headIndex = partIndex
+                BODY -> bodyIndex = partIndex
+                LEG -> legIndex = partIndex
+                else -> {}
+            }
+
+            val bundle = Bundle()
+            bundle.putInt(AndroidMeActivity.HEAD_TAG, headIndex)
+            bundle.putInt(AndroidMeActivity.BODY_TAG, bodyIndex)
+            bundle.putInt(AndroidMeActivity.LEG_TAG, legIndex)
+
+            val intent = Intent(this, AndroidMeActivity::class.java)
+            intent.putExtras(bundle)
+
+            val nextButton: Button = findViewById(R.id.next_button) as Button
+            nextButton.setOnClickListener({ _ -> startActivity(intent) })
         }
     }
 }
